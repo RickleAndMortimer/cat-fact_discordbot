6ed929b64019d7c118cde693cca9dee23a867947
RickleAndMortimer <33658638+RickleAndMortimer@users.noreply.github.com>
2020-06-09 13:05:28 -0400

    worked on database.py

--- database.py
+++ database.py
@@ -1,8 +1,39 @@
 import psycopg2
-
-user =
-database =
-port =
-password =
-
-
+##PostgreSQL connection info
+host = "localhost"
+user = "postgres"
+database = "cat_clients"
+port = "5432"
+password = "cupcakes29"
+##connects to postgreSQL server
+conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+##SQL commands
+#executes PostgreSQL commands
+def __executeCommand(command):
+    cur = conn.cursor() 
+    return cur.execute(command)
+#finds user in table
+def __findUser(userID):
+    if (__executeCommand("SELECT userID FROM users WHERE userID = {$0.userID};".format(userID):
+        return true
+    else:
+        return false
+#lists users in 
+#deletes userID in the table
+def deleteUser(userID):
+    exists = __findUser(userID)
+    if (exists):
+        __executeCommand("DELETE FROM users WHERE userID = {$0.userID};".format(userID))
+        return 1
+    else:
+        return -1
+#adds userID into the table
+def addUser(userID):
+    exists = __findUser(userID)
+    if (not exists):
+        __executeCommand("INSERT INTO users (userID) VALUES ($0.userID);".format(userID))
+        return 1
+    else:
+        return -1
+#test
+executeCommand(connect(host, user, database, port, password), "SELECT version()")
--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import discord
 from discord.ext import commands
 import importlib
+cat_facts = importlib.import_module('cat_facts')
+database = importlib.import_module('database')
 
 bot = commands.Bot(command_prefix='!')
 token = 'NzE5NzYzODAwNzM1MDg4NzY4.Xt8KoA.ufoKFOV8XNhYfbsxXAzDx78RuKE'
 
+##events
 @bot.event
 async def on_ready():
-    print("Logged in as {0.user}")
+    print("Logged in as {0.user}".format(bot))
 
-@bot.command()
-#gets info from cat-facts api and sends it to user
-async def sendFact(ctx):
-    await ctx.send("Fact sent!")
-
+##functions
+#sends fact to user
+async def sendfact(ctx):
+    await ctx.send(cat_facts.reqFact())
+#makes sure the command is from a user
+def validateCommand(ctx):
+    if (ctx.author.bot is discord.User.bot):
+        return -1
+    else:
+        return 1
+##commands
 @bot.command()
 #registers user to PostgreSQL database
 async def subscribe(ctx):
-    await ctx.send("Subscription successful! Enjoy your daily feed of cat facts!")
-#runs the bot
+    if (validateCommand(ctx) == 1): 
+        userID = ctx.author.id
+        if (database.addUser(userID) == 1):
+            await ctx.send("Subscription successful! Enjoy your daily feed of cat facts at 12:00 PM!")
+        else:
+            await ctx.send("ERROR: You're already subscribed to Crazy Cat Facts!")
+@bot.command()
+#unsubscribes user in PostgreSQL database
+async def unsubscribe(ctx):
+    if (validateCommand(ctx) == 1):
+        if (database.removeUser(userID) == 1):
+            await ctx.send("You have unsubscribed from Crazy Cat Facts :(.")
+        else:
+            await ctx.send("ERROR: You are not subscribed to Crazy Cat Facts.")        
+##runs the bot
 bot.run(token)
